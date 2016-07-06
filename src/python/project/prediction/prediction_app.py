348e36d45984b02b5e4e4e4fb15813b8458deb52
Dat Tran <datitran@gmail.com>
2016-07-06 15:27:24 +0200

    Add additional tests and refactored the code.

--- src/python/project/prediction/prediction_app.py
+++ src/python/project/prediction/prediction_app.py
@@ -1,25 +1,50 @@
+import os
+import redis
+import numpy as np
 from flask import Flask, request
 from PIL import Image, ImageOps
-import numpy as np
+from keras.models import model_from_json
+from keras.optimizers import RMSprop
 
 app = Flask(__name__)
+r = redis.StrictRedis(host="127.0.0.1", port=6379, db=0)
+
+def get_model(redis):
+    """Get model from redis and compile it."""
+    model = model_from_json(redis.get("model").decode("UTF-8"))
+    weights = redis.get("weights")
+    with open("mnist_mlp_weights.h5", "wb") as f:
+        f.write(weights)
+    model.load_weights("mnist_mlp_weights.h5")
+    os.remove("mnist_mlp_weights.h5")
+    model.compile(loss="categorical_crossentropy",
+              optimizer=RMSprop())
+    return model
+
+def convert_image(image):
+    """Invert the image and then transform it to 1-d vector."""
+    img = Image.open(image).convert("L")
+    inverted_img = ImageOps.invert(img)
+    data = np.asarray(inverted_img, dtype="int32")
+    rescaled_data = (data/255).reshape(28,28)
+    stacked_data = np.vstack([rescaled_data.reshape(-1)])
+    return stacked_data
 
 @app.route("/")
 def main():
-    return "Test"
+    return "Hello World!"
 
-@app.route("/prediction", methods=["GET", "POST"])
+@app.route("/prediction", methods=["POST"])
 def prediction():
     """
     curl -i -X POST -F files=@four_test.png http://127.0.0.1:5000/prediction
     """
     if request.method == "POST":
-        f = request.files["files"]
-        img = Image.open(f).convert("L")
-        data = np.asarray(img, dtype="int32")
-        return "200"
-    else:
-        return "201"
+        image = request.files["files"]
+        data = convert_image(image)
+        model = get_model(r)
+        prediction = model.predict_classes(data)
+        return str(prediction[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/python/tests/test_prediction_app.py
+++ src/python/tests/test_prediction_app.py
@@ -1,22 +1,47 @@
-from project.prediction.prediction_app import app
-import unittest
 import os
-from PIL import Image, ImageOps
-import numpy as np
+import unittest
+import project.prediction.prediction_app as prediction_app
+from project.prediction.prediction_app import app
+from unittest.mock import patch
+from keras.models import model_from_json
+from keras.optimizers import RMSprop
 
+def mock_get_model(redis=None):
+    model = model_from_json(open(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "data/mnist_mlp.json")).read())
+    model.load_weights(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "data/mnist_mlp_weights.h5"))
+    model.compile(loss="categorical_crossentropy",
+              optimizer=RMSprop())
+    return model
+
+@patch("project.prediction.prediction_app.get_model", mock_get_model)
 class PredictionAppTest(unittest.TestCase):
 
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-
-    def tearDown(self):
-        pass
+        self.files = {"files": open(os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "data/four_test.png"), "rb")}
+        self.image = os.path.join(os.path.dirname(
+            os.path.abspath(__file__)), "data/four_test.png")
 
     def test_main_page(self):
+        """Test that the status code 200 is returned for get."""
         results = self.app.get("/")
         self.assertEqual(results.status_code, 200)
 
-    def test_prediction(self):
-        results = self.app.get("/prediction")
+    def test_convert_image(self):
+        """Test if the image was converted correctly."""
+        converted_image = prediction_app.convert_image(self.image)
+        self.assertAlmostEqual(converted_image.sum(), 46.129411764)
+
+    def test_prediction_status(self):
+        """Test that the status code 200 is returned for post."""
+        results = self.app.post("/prediction", data=self.files)
         self.assertEqual(results.status_code, 200)
+
+    def test_prediction_results(self):
+        """Test that the right prediction is returned."""
+        results = self.app.post("/prediction", data=self.files)
+        self.assertEqual(results.get_data(as_text=True), "4")
